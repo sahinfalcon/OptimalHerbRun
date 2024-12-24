e0b78800b9e8c2b801e0cdc0b271cdbcbea27de5
Taylan Sahin <info@taylansahin.net>
2024-12-24 16:18:52 +0000

    add HerbType enum

---
+++ src/main/java/com/optimalHerbRun/data/HerbType.java
@@ -0,0 +1,18 @@
+package com.optimalHerbRun.data;
+
+public enum HerbType {
+    GUAM,
+    MARRENTILL,
+    TARROMIN,
+    HARRALANDER,
+    RANARR,
+    TOADFLAX,
+    IRIT,
+    AVANTOE,
+    KWUARM,
+    SNAPDRAGON,
+    CADANTINE,
+    LANTADYME,
+    DWARF_WEED,
+    TORSTOL
+}
