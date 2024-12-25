c5354f4bfbe1cee7d39daa053ee173390387540f
Taylan Sahin <info@taylansahin.net>
2024-12-25 12:58:11 +0000

    add setter for disease state

--- src/main/java/com/optimalHerbRun/data/HerbPatch.java
+++ src/main/java/com/optimalHerbRun/data/HerbPatch.java
@@ -1,13 +1,20 @@
 package com.optimalHerbRun.data;
 
+import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.coords.WorldPoint;
 import java.time.Instant;
 import lombok.Data;
 
+@Slf4j
 @Data
 public class HerbPatch {
     private final WorldPoint location;
     private Instant plantTime;
     private HerbType type;
     private boolean isDiseased;
+
+    public void setDiseased(boolean diseased){
+        this.isDiseased = diseased;
+        log.info("Patch at {} disease state changed to: {}", location, diseased);
+    }
 }
